0c4fa02560e5a02062df54290f9f64f786b294a3
subodh <pdsubodh@gmail.com>
2026-04-03 12:55:46 +0530

    Most receient

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ def multiply(a, b):
 if __name__ == "__main__":
     a = 12
     b = 3
-    print("Hello")
+    print("My Commmit - 12:55")
     print(f"\nMultiplication of {a} & {b} is : {multiply(a, b)}\n")
