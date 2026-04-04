a644b2d1c710feb084bb5a56ffd438c1121bd33b
subodh <pdsubodh@gmail.com>
2026-04-05 01:19:51 +0530

    Added - 4

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ def multiply(a, b):
 if __name__ == "__main__":
     a = 2
     b = 8
-    #hello World    
+    #hello World 123
     print(f"\nMultiplication of {a} & {b} is :: {multiply(a, b)}\n")
